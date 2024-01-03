ef28334a6befc5490b6d3a4c60c2f39e28f86f29
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2024-01-03 19:12:10 +0100

    start integration of django vite plungin

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import { djangoVitePlugin } from 'django-vite-plugin'
+import { globSync } from 'glob'
+
+export default defineConfig({
+    plugins: [
+        djangoVitePlugin([])
+    ],
+});
